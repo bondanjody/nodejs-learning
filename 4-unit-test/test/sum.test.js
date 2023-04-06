27446ba7f04108122481ed2e6118fca3c0719678
bondanjody <bondanjodys@gmail.com>
2023-04-07 03:05:51 +0700

    Menambahkan konfigurasi pada Jest. Sebenarnya kita memiliki opsi lain untuk mengatur konfigurasi Jest, yaitu dengan file terpisah jest.config.json yang mana dapat kita generate dengan command : npx jest --init . Namun disini kita hanya akan menggunakan konfigurasi Jest yang diletakkan pada file package.json. Pada kasus ini kita menggunakan konfigurasi tambahan yaitu 'verbose' yang digunakan untuk menampilkan hasil test secara mendetail. Namun, hasil dari verbose ini baru terlihat perbedaanya (daripada testing default) apabila kita melakukan testing untuk 2-3 fungsi (banyak fungsi)

--- 4-unit-test/test/sum.test.js
+++ 4-unit-test/test/sum.test.js
@@ -1,8 +1,15 @@
 import { sum } from "../src/sum";
 
-test("sum(1,2) harusnya hasilnya 3", () => {
+test("Tes sum function 1", () => {
   const result = sum(1, 2);
   expect(result).toBe(3);
 });
-
+test("Tes sum function 2", () => {
+  const result = sum(1, 2);
+  expect(result).toBe(3);
+});
+test("Tes sum function 3", () => {
+  const result = sum(1, 2);
+  expect(result).toBe(3);
+});
 // Parameter pertama pada 'test' adalah nama yang kita sesuaikan sendiri, lalu parameter kedua adalah callback untuk melakukan testing
