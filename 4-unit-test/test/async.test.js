28821d9e27a480fbc6d041d0155ef203d9d1eb51
bondanjody <bondanjodys@gmail.com>
2023-04-07 05:38:22 +0700

    Menambahkan kode program untuk Async Matchers

--- 4-unit-test/test/async.test.js
+++ 4-unit-test/test/async.test.js
@@ -1,6 +1,15 @@
 import { sayHello } from "../src/async";
 
+// testing async code
 test("test async function", async () => {
   const result = await sayHello("Bondan");
   expect(result).toBe("Hello, Bondan !");
 });
+
+// async matchers
+test("async matchers", async () => {
+  await expect(sayHello("Bondan")).resolves.toBe("Hello, Bondan !");
+  await expect(sayHello()).rejects.toBe("Invalid NAME!"); // parameter toBe harus sama dengan nilai reject pada async.js
+});
+
+// NOTE : Setelah resolves.<boleh diisi dengan matcher apa saja, termasuk NOT>
