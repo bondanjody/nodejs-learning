67e24dcc7d836f55b90c366df167ce12ff9bb0f4
bondanjody <bondanjodys@gmail.com>
2023-04-07 02:53:43 +0700

    Melakukan integrasi Babel dengan Jest. Sekarang jika kita menjalankan command : npx jest . Maka sudah akan berjalan dengan semestinya

--- 4-unit-test/test/sum.test.js
+++ 4-unit-test/test/sum.test.js
@@ -4,3 +4,5 @@ test("sum(1,2) harusnya hasilnya 3", () => {
   const result = sum(1, 2);
   expect(result).toBe(3);
 });
+
+// Parameter pertama pada 'test' adalah nama yang kita sesuaikan sendiri, lalu parameter kedua adalah callback untuk melakukan testing
