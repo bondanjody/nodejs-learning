6d1b345059d1d1a54420af8bc0cd037adb405634
bondanjody <bondanjodys@gmail.com>
2023-04-07 03:22:14 +0700

    Menambahkan file equal.test.js. Pada file ini kita mempraktikkan Equal Matchers (toBe, toEqual). NOTE: toEqual misalnya digunakan pada object

---
+++ 4-unit-test/src/equal.test.js
@@ -0,0 +1,11 @@
+test("test toBe", () => {
+  const nama = "Bondan";
+  const hello = `Hello, ${nama}`;
+  expect(hello).toBe("Hello, Bondan");
+});
+
+test("test toEqual", () => {
+  let person = { id: "p2" };
+  Object.assign(person, { nama: "Bondan" });
+  expect(person).toEqual({ id: "p1", nama: "Bondan" });
+});
