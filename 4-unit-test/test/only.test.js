6deda0f9af152abbe061f3b7f6bdbf6c0cdaaac5
bondanjody <bondanjodys@gmail.com>
2023-04-07 21:09:30 +0700

    Menambahkan file only.test.js yang mendemokan Only Function (yaitu function apa yang hanya akan dijalankan/ditest)

--- 4-unit-test/test/only.test.js
+++ 4-unit-test/test/only.test.js
@@ -1,7 +1,8 @@
 test("test1", () => console.log("Test1"));
 test("test2", () => console.log("Test2"));
-test.skip("test3", () => console.log("Test3"));
+test.only("test3", () => console.log("Test3"));
 test("test4", () => console.log("Test4"));
 test("test5", () => console.log("Test5"));
 
 // NOTE : Juga bisa diaplikasikan bila kita menggunakan 'it'
+// Kebalikan dari skip function, hanya yang memiliki 'only' yang akan di test
