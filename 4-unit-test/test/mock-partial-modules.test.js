f5b2fad1e7af7cc558297a191a98940a7f8974aa
bondanjody <bondanjodys@gmail.com>
2023-04-08 00:29:26 +0700

    Menyempurnakan Mock partial Module

--- 4-unit-test/test/mock-partial-modules.test.js
+++ 4-unit-test/test/mock-partial-modules.test.js
@@ -2,7 +2,7 @@ import { getAllProducts } from "../src/database";
 import { ProductService } from "../src/product-service";
 
 jest.mock("../src/database.js", () => {
-  const originalModule = jest.requireActual("../src/database.js");
+  const originalModule = jest.requireActual("../src/database.js"); // Memanggil module aslinya
 
   return {
     __esModule: true,
