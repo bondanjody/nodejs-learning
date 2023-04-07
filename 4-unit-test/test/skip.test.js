38fb57a38a6bae26bf01a7ad769906e55fbe82f0
bondanjody <bondanjodys@gmail.com>
2023-04-07 21:05:30 +0700

    Menambahkan 'skip.test.js' yang mendemonstrasikan bagaimana kita bisa men-skip suatu test

---
+++ 4-unit-test/test/skip.test.js
@@ -0,0 +1,7 @@
+test("test1", () => console.log("Test1"));
+test("test2", () => console.log("Test2"));
+test.skip("test3", () => console.log("Test3"));
+test("test4", () => console.log("Test4"));
+test("test5", () => console.log("Test5"));
+
+// NOTE : Juka bisa diaplikasikan bila kita menggunakan 'it'
