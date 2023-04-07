6621c1c33006e6ad28d2c9661198f6c62bac32a0
bondanjody <bondanjodys@gmail.com>
2023-04-08 01:50:03 +0700

    Menset level

--- 5-logging/test/logging-level.test.js
+++ 5-logging/test/logging-level.test.js
@@ -13,3 +13,19 @@ test("logging with level", () => {
   logger.log({ level: "debug", message: "Debug Message" });
   logger.log({ level: "silly", message: "Silly Message" });
 });
+
+test("logging with level and setup the level", () => {
+  const logger = winston.createLogger({
+    level: "debug",
+    transports: [new winston.transports.Console({})],
+  });
+
+  logger.log({ level: "error", message: "Error Message" });
+  logger.log({ level: "warn", message: "Warn Message" });
+  logger.log({ level: "info", message: "Info Message" });
+  logger.log({ level: "http", message: "HTTP Message" });
+  logger.log({ level: "verbose", message: "Verbose Message" });
+  logger.log({ level: "debug", message: "Debug Message" });
+  logger.log({ level: "silly", message: "Silly Message" });
+});
+// NOTE : kode di atas akan menampilkan dari level debug ke atas
