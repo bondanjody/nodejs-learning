05614b115a38eebffbb8795f6a029e5d787e880d
bondanjody <bondanjodys@gmail.com>
2023-04-07 21:29:19 +0700

    Kita juga bisa menggunakan Object pada Each Function, serta memanfaatkan fitur destructuring

--- 4-unit-test/test/each.test.js
+++ 4-unit-test/test/each.test.js
@@ -19,3 +19,16 @@ test.each(table)("test sumAll(%s) should results %i", (numbers, expected) => {
   expect(sumAll(numbers)).toBe(expected);
 });
 // numbers akan menyimpan array [10,10,10], expected akan menyimpan nilai 30  (array pertama), begitu seterusnya
+
+// Kita juga bisa menggunakan Object pada Each Function, serta memanfaatkan fitur destructuring
+const tableObject = [
+  { numbers: [10, 10, 10], expected: 30 },
+  { numbers: [10, 10, 10, 10], expected: 40 },
+];
+
+test.each(tableObject)(
+  "test object data sumAll($numbers) should results %expected",
+  ({ numbers, expected }) => {
+    expect(sumAll(numbers)).toBe(expected);
+  }
+);
