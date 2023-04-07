aa5f6d11cd29ab2e80e7af7f76fbc9f699e49e21
bondanjody <bondanjodys@gmail.com>
2023-04-08 06:01:40 +0700

    Menangkap query parameter yang dikirim

---
+++ 6-express/test/request-query.test.js
@@ -0,0 +1,15 @@
+import express from "express";
+import request from "supertest";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`Hello ${req.query.firstname} ${req.query.lastname} !`);
+});
+
+test("test request", async () => {
+  const response = await request(app)
+    .get("/")
+    .query({ firstname: "Bondan", lastname: "Jody" });
+  expect(response.text).toBe("Hello Bondan Jody !");
+});
