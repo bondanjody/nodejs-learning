7c2f6f2b9ecb468f28fd77904e16787276bf5e4e
bondanjody <bondanjodys@gmail.com>
2023-04-08 01:46:03 +0700

    Menggunakan Level untuk menampilkan seberapa penting log. Namun hanya error, warn, dan info saja yang ditampilkan

---
+++ 5-logging/test/logging-level.test.js
@@ -0,0 +1,15 @@
+import winston from "winston";
+
+test("logging with level", () => {
+  const logger = winston.createLogger({
+    transports: [new winston.transports.Console({})],
+  });
+
+  logger.log({ level: "error", message: "Error Message" });
+  logger.log({ level: "warn", message: "Warn Message" });
+  logger.log({ level: "info", message: "Info Message" });
+  logger.log({ level: "http", message: "HTTP Message" });
+  logger.log({ level: "verbose", message: "Verbose Message" });
+  logger.log({ level: "debug", message: "Debug Message" });
+  logger.log({ level: "silly", message: "Silly Message" });
+});
