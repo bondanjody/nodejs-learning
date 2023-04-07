54b5592339d7388989468ed6ccc06e995f14c01f
bondanjody <bondanjodys@gmail.com>
2023-04-08 01:54:37 +0700

    Menggunakan shortcut function

--- 5-logging/test/logging-level.test.js
+++ 5-logging/test/logging-level.test.js
@@ -14,6 +14,7 @@ test("logging with level", () => {
   logger.log({ level: "silly", message: "Silly Message" });
 });
 
+// setelah level di set
 test("logging with level and setup the level", () => {
   const logger = winston.createLogger({
     level: "debug",
@@ -29,3 +30,19 @@ test("logging with level and setup the level", () => {
   logger.log({ level: "silly", message: "Silly Message" });
 });
 // NOTE : kode di atas akan menampilkan dari level debug ke atas
+
+// shortcut function
+test("using shortcut", () => {
+  const logger = winston.createLogger({
+    level: "silly",
+    transports: [new winston.transports.Console({})],
+  });
+
+  logger.error("Error Message");
+  logger.warn("Warn Message");
+  logger.info("Info Message");
+  logger.http("HTTP Message");
+  logger.verbose("Verbose Message");
+  logger.debug("Debug Message");
+  logger.silly("Silly Message");
+});
