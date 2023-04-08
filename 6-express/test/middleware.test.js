98f025564bbe81a9bdc88972e627b54710f0c7c5
bondanjody <bondanjodys@gmail.com>
2023-04-08 07:01:41 +0700

    Menambahkan middleware apiKeyMiddleware

--- 6-express/test/middleware.test.js
+++ 6-express/test/middleware.test.js
@@ -11,9 +11,18 @@ function addPoweredHeader(req, res, next) {
   next();
 }
 
+function apiKeyMiddleware(req, res, next) {
+  if (req.query.apiKey) {
+    next();
+  } else {
+    res.status(401).end();
+  }
+}
+
 const app = express();
 
 app.use(logger);
+app.use(apiKeyMiddleware);
 app.use(addPoweredHeader);
 
 app.get("/", (req, res) => {
@@ -25,11 +34,17 @@ app.get("/about", (req, res) => {
 });
 
 test("test response middleware 1", async () => {
-  const response = await request(app).get("/");
+  const response = await request(app).get("/").query({ apiKey: "34a" });
   expect(response.get("X-Powered-By")).toBe("Google Inc");
 });
 
 test("test response middleware 2", async () => {
-  const response = await request(app).get("/about");
+  const response = await request(app).get("/about").query({ apiKey: "35b" });
   expect(response.get("X-Powered-By")).toBe("Google Inc");
+});
+// NOTE : sekarang jika tidak ada query.apiKey maka akan error
+
+test("test response middleware unauthorized", async () => {
+  const response = await request(app).get("/about");
+  expect(response.status).toBe(401);
 });
