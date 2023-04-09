99ea8f6bd2a3c371cc2a34c1caefb993e2778611
bondanjody <bondanjodys@gmail.com>
2023-04-09 09:03:58 +0700

    Menggunakan prefix path

--- 6-express/test/static-file.test.js
+++ 6-express/test/static-file.test.js
@@ -2,9 +2,14 @@ import express from "express";
 import request from "supertest";
 
 const app = express();
-app.use(express.static(__dirname + "/static"));
+// app.use(express.static(__dirname + "/static"));
+app.use("/static", express.static(__dirname + "/static"));
 
 app.get("/", (req, res) => {
+  res.send(`Hello Response !`);
+});
+
+app.get("/contoh.txt", (req, res) => {
   res.send(`Hello Response !`);
 });
 
@@ -13,7 +18,14 @@ test("test static file ", async () => {
   expect(response.text).toBe("Hello Response !");
 });
 
+// Ini akan masuk ke routing '/contoh.txt'
 test("test static file contoh.txt", async () => {
   const response = await request(app).get("/contoh.txt");
+  expect(response.text).toContain("Hello Response !");
+});
+
+// Ini akan masuk ke routing '/static/contoh.txt'  (yang didefinisikan pada middleware)
+test("test static file /static/contoh.txt", async () => {
+  const response = await request(app).get("/static/contoh.txt");
   expect(response.text).toContain("Sample text");
 });
