3b77fc955b3d6d7226fe917a38f74768dfa4a724
bondanjody <bondanjodys@gmail.com>
2023-04-09 17:24:32 +0700

    Mendemokan penggunaan Mustache Cache, terutama function parse

--- 7-mustache/test/mustache.test.js
+++ 7-mustache/test/mustache.test.js
@@ -4,3 +4,10 @@ test("Menggunakan Mustache", () => {
   const data = Mustache.render("Hello {{name}}", { name: "Bondan" });
   expect(data).toBe("Hello Bondan");
 });
+
+// Menggunakan Mustache Cache
+test("Menggunakan Mustache Cache", () => {
+  Mustache.parse("Hello {{name}}");
+  const data = Mustache.render("Hello {{name}}", { name: "Bondan" });
+  expect(data).toBe("Hello Bondan");
+});
