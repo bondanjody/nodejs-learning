8d2c62efc93b49d77eda745749d79926f1cb961d
bondanjody <bondanjodys@gmail.com>
2023-04-09 16:52:01 +0700

    Menginstall package express-fileupload. Mendemokan cara melakukan file upload di express

--- 6-express/test/request-body.test.js
+++ 6-express/test/request-body.test.js
@@ -1,9 +1,11 @@
 import express from "express";
 import request from "supertest";
+import expressFileUpload from "express-fileupload"; // Menggunakan express-fileupload
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(expressFileUpload());
 // Di set false supaya tidak digabung dengan body
 
 app.post("/json", (req, res) => {
@@ -11,9 +13,27 @@ app.post("/json", (req, res) => {
   res.json({ hello: `Hello, ${name} !` });
 });
 
+// Menggunakan express file upload
+app.post("/file", async (req, res) => {
+  const textFile = req.files.article;
+  await textFile.mv(__dirname + "/upload/" + textFile.name);
+
+  res.send(`Hi ${req.body.name}, You've uploaded ${textFile.name}`);
+});
+
 app.post("/form", (req, res) => {
   const name = req.body.name;
   res.json({ hello: `Hello, ${name} !` });
+});
+
+// Mengetes express file upload
+test("test request express file upload", async () => {
+  let response = await request(app)
+    .post("/file")
+    .set("Content-Type", "multipart/form-data")
+    .field("name", "Bondan")
+    .attach("article", __dirname + "/contoh.txt");
+  expect(response.text).toBe("Hi Bondan, You've uploaded contoh.txt");
 });
 
 // Mengetes middleware json
