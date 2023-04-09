322af80d3dcc71024cd6b07cd63e2da6c25d5ec5
bondanjody <bondanjodys@gmail.com>
2023-04-09 17:29:08 +0700

    Menambahkan tag HTML ke dalam Mustache

--- 7-mustache/test/mustache.test.js
+++ 7-mustache/test/mustache.test.js
@@ -11,3 +11,12 @@ test("Menggunakan Mustache Cache", () => {
   const data = Mustache.render("Hello {{name}}", { name: "Bondan" });
   expect(data).toBe("Hello Bondan");
 });
+
+// Menggunakan Tag HTML di dalam Mustache
+test("Menggunakan Tag HTML di dalam Mustache", () => {
+  const data = Mustache.render(
+    "Hello my name is {{name}}, and my hobby is {{{hobby}}}",
+    { name: "Bondan", hobby: "programming" }
+  );
+  expect(data).toBe("Hello my name is Bondan, and my hobby is programming");
+});
